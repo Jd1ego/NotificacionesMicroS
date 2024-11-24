b1c923989d344f2fb0e90d80b4900bf14db87f29
Jd1ego <140413545+Jd1ego@users.noreply.github.com>
2024-11-24 12:23:41 -0500

    Primer commit

---
+++ src/main/java/com/example/NotificacionesMicroservicio/NotificacionesMicroservicioApplication.java
@@ -0,0 +1,13 @@
+package com.example.NotificacionesMicroservicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificacionesMicroservicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificacionesMicroservicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NotificacionesMicroservicio/NotificacionesMicroservicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NotificacionesMicroservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacionesMicroservicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
